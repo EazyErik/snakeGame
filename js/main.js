6aa04fb05ad4e542dbce413a023860b6b787d2d5
erik busch <eazyErik>
2024-01-27 13:51:58 +0100

    snake can eat item

--- js/main.js
+++ js/main.js
@@ -1,46 +1,87 @@
+class Item {
+    constructor() {
+        this.width = 40;
+        this.height = 40;
+        this.positionX = this.getRandomNumber(0,window.innerWidth);
+        this.positionY = this.getRandomNumber(0,window.innerHeight);
+
+        this.createDomElement();
+
+    }
+
+    getRandomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    createDomElement() {
+        this.domElement = document.createElement("div");
+        this.domElement.setAttribute("id", "item");
+        this.domElement.style.width = this.width + "px";
+        this.domElement.style.height = this.height + "px";
+        this.domElement.style.left = this.positionX + "px";
+        this.domElement.style.bottom = this.positionY + "px";
+        const board = document.querySelector("#board");
+        board.appendChild(this.domElement);
+
+
+    }
+
+    eat(){
+        this.domElement.remove();
+
+    }
+    
+}
+
+
+
 class Player {
     constructor() {
-        this.width = 10;
-        this.height = 10;
+        this.width = 40;
+        this.height = 40;
         this.positionX = 0;
         this.positionY = 0;
+        this.position = [{ positionX: 0, positionY: 0 }];
         this.createDomElement();
         this.direction = null;
         this.isMoving = false;
         this.moveInterval = null;
         this.handleKeyDown = this.handleKeyDown.bind(this);
         this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.item = new Item();
+        
+    
 
     }
 
     moveLeft() {
-        console.log("moving left");
+     
         this.direction = "left";
         this.startMoving();
 
     }
     moveRight() {
-        console.log("moving right");
+       
         this.direction = "right";
         this.startMoving();
 
     }
     moveUp() {
-        this.positionY += 10;
-        this.domElement.style.bottom = this.positionY + "vw";
+        this.direction = "up";
+        this.startMoving();
     }
     moveDown() {
-        this.positionY -= 10;
-        this.domElement.style.bottom = this.positionY + "vw";
+        this.direction = "down";
+        this.startMoving();
     }
 
     createDomElement() {
         this.domElement = document.createElement("div");
         this.domElement.setAttribute("id", "player");
-        this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh";
-        this.domElement.style.left = this.positionX + "vw";
-        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.width = this.width + "px";
+        this.domElement.style.height = this.height + "px";
+        this.domElement.style.left = this.position[0].positionX + "px";
+        this.domElement.style.bottom = this.position[0].positionY + "px";
         const board = document.querySelector("#board");
         board.appendChild(this.domElement);
 
@@ -51,12 +92,35 @@ class Player {
             this.isMoving = true;
             this.moveInterval = setInterval(() => {
                 if (this.direction === "left") {
-                    this.positionX -= 1;
-                } else if (this.direction === "right") {
-                    this.positionX += 1;
-                }
+                    for(let i = this.position.length -1 ; i > 0; i--){
+                        this.position[i] = position[i -1];
+                    }
 
-                this.domElement.style.left = this.positionX + "vw";
+                    this.position[0].positionX -= 20;
+                } else if (this.direction === "right") {
+                    for(let i = this.position.length -1 ; i > 0; i--){
+                        this.position[i] = position[i -1];
+                    }
+                    this.position[0].positionX += 20;
+
+                } else if (this.direction === "up") {
+                    for(let i = this.position.length -1 ; i > 0; i--){
+                        this.position[i] = position[i -1];
+                    }
+                    this.position[0].positionY += 20;
+                }
+                else if (this.direction === "down") {
+                    for(let i = this.position.length -1 ; i > 0; i--){
+                        this.position[i] = position[i -1];
+                    }
+                    this.position[0].positionY -= 20;
+                }
+                //todo: Koerperbewegung hinzufuegen.
+                this.domElement.style.left = this.position[0].positionX + "px";
+                this.domElement.style.bottom = this.position[0].positionY + "px";
+                this.detectCollision();
+
+                
             }, 20);
         }
     }
@@ -84,6 +148,16 @@ class Player {
         }
     }
 
+    detectCollision(){
+        if(    this.position[0].positionX < this.item.positionX + this.item.width &&
+            this.position[0].positionX + this.width > this.item.positionX &&
+            this.position[0].positionY < this.item.positionY + this.item.height &&
+            this.position[0].positionY + this.height > this.item.positionY){
+                this.item.eat();
+                this.item = new Item();
+            }
+    }
+
 
 }
 
@@ -94,7 +168,7 @@ class Player {
 const player = new Player();
 
 document.addEventListener("keydown", (event) => {
-    console.log(event);
+ 
     switch (event.code) {
         case "ArrowLeft":
             player.moveLeft();
@@ -116,5 +190,9 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keyup", () => {
     player.stopMoving();
 });
+
+
+
+
 
 
