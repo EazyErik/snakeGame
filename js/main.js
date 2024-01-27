a5ab55fea34340d752d65e2a6873284d44353ef4
erik busch <eazyErik>
2024-01-27 10:03:07 +0100

    create player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("Keep going!"); 
+class Player{
+    constructor(){
+        this.width = 10;
+        this.height = 10;
+        this.positionX = 0;
+        this.positionY = 0;
+
+        const domElement = document.createElement("div");
+        domElement.setAttribute("id","player");
+        const board = document.querySelector("#board");
+        board.appendChild(domElement);
+    }
+
+    moveLeft(){
+        this.positionX -= 10;
+    }
+    moveRight(){
+        this.positionX += 10;
+    }
+
+    
+}
+
+const player = new Player();
+
